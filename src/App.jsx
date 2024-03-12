1119fbe28d558b2243c459289dd00a83dabc8c08
wenciu <72387149+wenciu@users.noreply.github.com>
2024-03-12 16:51:29 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-
+import InventoryList from "./components/inventoryList";
 
 function App() {
-
   return (
     <>
-      <h1
-        className="text-3xl text-center text-red-700"
-      >Welcome to Vite with TailwindCSS and React</h1>
+      <InventoryList />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/InventoryList.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function inventoryList() {
+  return (
+    <>
+      <div>inventoryList</div>
+    </>
+  );
+}
+
+export default inventoryList;
